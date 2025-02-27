918801adb3e7ceb61e26c285404e60444add5517
Mekd <mekdelawitandualem07@gmail.com>
2025-02-27 22:33:53 +0300

    first commit

---
+++ landing-page/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './component/NavBar'; 
+
+export default function App() {
+
+  return (
+    <>
+     <NavBar/>
+
+    </>
+  )
+}
+
+
---
+++ landing-page/src/component/NavBar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar(){
+    return(
+        <ul className="NavBar-links">
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+
+            <button className="SignUp-btn">SignUp</button>
+        </ul>
+    );
+}
