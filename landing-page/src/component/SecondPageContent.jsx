dce9284552e09c96f80a961a1ed7425c07f946f9
Mekd <mekdelawitandualem07@gmail.com>
2025-03-03 20:40:48 +0300

    fifth commit

--- landing-page/src/component/SecondPageContent.jsx
+++ landing-page/src/component/SecondPageContent.jsx
@@ -1,7 +1,8 @@
 import '../style.css'
 import { LineChartOutlined } from '@ant-design/icons';
 import { CodeSandboxOutlined } from '@ant-design/icons';
-import { ShieldOutlined } from '@ant-design/icons';
+import shield_icon_pic from '../images/shield_icon_pic.jpg';
+
 import { ToolOutlined } from '@ant-design/icons';
 
 export default function SecondPageContent(){
@@ -21,25 +22,25 @@ export default function SecondPageContent(){
                 </section>
 
                 <section className='p-8 rounded-2xl shadow bg-teal-900 bg-opacity-50 h-70'>
-                <ToolOutlined style={{ fontSize: '24px', color: 'white' }} />
-                    <h3 className='font-semibold text-2xl pt-30'>Quality Control</h3>
+                <ToolOutlined style={{ fontSize: '38px', color: 'white' }} />
+                    <h3 className='font-semibold text-2xl pt-15'>Quality Control</h3>
                     <p>Procedures and systems in place to ensure high product quality.</p>
                 </section>
 
                 <section className='p-8 rounded-2xl shadow bg-teal-900 bg-opacity-50 h-70'>
-                    <ShieldOutlined style={{ fontSize: '24px', color: 'white' }} />
-                    <h3 className='font-semibold text-2xl pt-30'>Technology and Innovation</h3>
+                    <img src={shield_icon_pic} alt="factory's pipe picture" className="w-12 h-auto rounded-4xl" />
+                    <h3 className='font-semibold text-2xl pt-10'>Technology and Innovation</h3>
                     <p>Details on the latest manufacturing technologies and ongoing innovations.</p>
                 </section>
 
                 <section className='p-8 rounded-2xl shadow bg-teal-900 bg-opacity-50 h-70'>
-                <CodeSandboxOutlined style={{ fontSize: '40px', color: 'white' }} />
+                <CodeSandboxOutlined style={{ fontSize: '38px', color: 'white' }} />
                     <h3 className='font-semibold text-2xl pt-11'>Packaging and Logistics</h3>
                     <p className='pt-2'>Packaging and logistics for shipping to customers and distributors. </p>
                 </section>
 
                 <section className='p-8 rounded-2xl shadow bg-teal-900 bg-opacity-50 h-70'>
-                <LineChartOutlined style={{ fontSize: '40px', color: 'white' }} />
+                <LineChartOutlined style={{ fontSize: '38px', color: 'white' }} />
                     <h3 className='font-semibold text-2xl pt-10'>Consulting Market Research</h3>
                     <p>Services to help companies understand market needs and provide strategic advice.</p>
                 </section>
